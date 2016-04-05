960b00a53d3e23a7987d68bfdca9abe87d9896e6
RLOC <rloc@itu.dk>
2016-04-05 13:22:44 +0200

    added init project structure

---
+++ 090-miniproject-1/todo.vb
@@ -0,0 +1,22 @@
+Cheat sheet for Mini project 1 - Advanced Programming
+
+Meet with Radek the following days:
+
+Tuesday 05-04-2016
+Sunday 10-04-2016
+(prob. +1)
+
+
+The project ---------
+
+stream00/Stream.scala --> ????
+
+stream01/Stream.scala --> contains example that violates property 1
+
+stream02/Stream.scala --> contains example that violates property 2
+
+To switch the file you are testing, uncomment a suitable import in
+the top of the StreamSpecWasowski file.  Note, that if you comment
+out all imports, you are testing the standard library's
+implementation of Streams (which is not what we want to do).
+
